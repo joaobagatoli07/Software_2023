dc5eacb455d0a5df1b85decdd4ee76ce366f4753
João Vitor Bagatoli <118355943+joaobagatoli07@users.noreply.github.com>
2023-05-20 08:57:17 -0300

    Add files via upload

---
+++ all_clean (1).sql
@@ -0,0 +1,196 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 20-Maio-2023 às 13:56
+-- Versão do servidor: 8.0.21
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `all_clean`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `contato`
+--
+
+CREATE TABLE `contato` (
+  `id_contato` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `assunto` varchar(100) NOT NULL,
+  `mensagem` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `forma_pagamento`
+--
+
+CREATE TABLE `forma_pagamento` (
+  `id_forma_pagamento` int NOT NULL,
+  `forma_pagamento` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `numero_cartao` int NOT NULL,
+  `codigo_seguranca_cartao` int NOT NULL,
+  `nome_cartao` varchar(100) NOT NULL,
+  `validade_cartao` date NOT NULL,
+  `quantidade_parcelas` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pedido`
+--
+
+CREATE TABLE `pedido` (
+  `id_pedido` int NOT NULL,
+  `id_planos` int NOT NULL,
+  `id_usuario` int NOT NULL,
+  `data_pedido` date NOT NULL,
+  `horario_pedido` time NOT NULL,
+  `valor_pedido` decimal(10,0) NOT NULL,
+  `endereco_pedido` varchar(100) NOT NULL,
+  `id_forma_pagamento` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `planos`
+--
+
+CREATE TABLE `planos` (
+  `id_planos` int NOT NULL,
+  `nome_plano` varchar(100) NOT NULL,
+  `descricao_plano` varchar(100) NOT NULL,
+  `valor_plano` decimal(10,2) NOT NULL,
+  `peso_plano` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int NOT NULL,
+  `nome_usuario` varchar(100) NOT NULL,
+  `telefone_usuario` varchar(100) NOT NULL,
+  `cpf_usuario` int NOT NULL,
+  `email_usuario` varchar(100) NOT NULL,
+  `senha_usuario` varchar(50) NOT NULL,
+  `data_cadastro_usuario` date NOT NULL,
+  `rua_usuario` varchar(100) NOT NULL,
+  `bairro_usuario` varchar(100) NOT NULL,
+  `numero_casa_usuario` int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `contato`
+--
+ALTER TABLE `contato`
+  ADD PRIMARY KEY (`id_contato`),
+  ADD KEY `fk_id_usuario2` (`id_usuario`);
+
+--
+-- Índices para tabela `forma_pagamento`
+--
+ALTER TABLE `forma_pagamento`
+  ADD PRIMARY KEY (`id_forma_pagamento`);
+
+--
+-- Índices para tabela `pedido`
+--
+ALTER TABLE `pedido`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `fk_id_planos` (`id_planos`),
+  ADD KEY `fk_id_usuario` (`id_usuario`),
+  ADD KEY `fk_id_forma_pagamento` (`id_forma_pagamento`);
+
+--
+-- Índices para tabela `planos`
+--
+ALTER TABLE `planos`
+  ADD PRIMARY KEY (`id_planos`);
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `contato`
+--
+ALTER TABLE `contato`
+  MODIFY `id_contato` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `forma_pagamento`
+--
+ALTER TABLE `forma_pagamento`
+  MODIFY `id_forma_pagamento` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `pedido`
+--
+ALTER TABLE `pedido`
+  MODIFY `id_pedido` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `planos`
+--
+ALTER TABLE `planos`
+  MODIFY `id_planos` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `contato`
+--
+ALTER TABLE `contato`
+  ADD CONSTRAINT `fk_id_usuario2` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+
+--
+-- Limitadores para a tabela `pedido`
+--
+ALTER TABLE `pedido`
+  ADD CONSTRAINT `fk_id_forma_pagamento` FOREIGN KEY (`id_forma_pagamento`) REFERENCES `forma_pagamento` (`id_forma_pagamento`),
+  ADD CONSTRAINT `fk_id_planos` FOREIGN KEY (`id_planos`) REFERENCES `planos` (`id_planos`),
+  ADD CONSTRAINT `fk_id_usuario` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
