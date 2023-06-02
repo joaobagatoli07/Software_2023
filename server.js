99426ca75eb176256995bf099cf0637c25a8b1c5
Lucas <lucas.campestrini21@gmail.com>
2023-06-02 09:14:23 -0300

    first

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/login.html')
 })
 
+
+
 app.post('/login', (req, res) => {
   let email = req.body.email;
   let senha = req.body.senha;
@@ -53,6 +55,44 @@ app.post('/login', (req, res) => {
     }
   });
 });
+
+
+
+app.get('/cadastro', (req, res) => {
+  res.sendFile(__dirname + '/pages/cadastro.html')
+})
+
+app.post('/cadastro', (req, res) => {
+  let email = req.body.email;
+  let senha = req.body.senha;
+
+  connection.query("insert * FROM usuario where email_usuario = '" + email + "'" , function (err, rows, fields) {
+    console.log("Results:", rows);
+    if (!err) {
+      if (rows.length > 0) {
+
+        if ( rows[0].senha_usuario  === senha) {
+            res.send('Login com Sucesso!!!');
+
+
+
+            
+            } else {
+             res.send('Senha incorreta');
+            }
+
+      } else {
+        res.send('Login Falhou - Email não cadastrado');
+      }
+    } else {
+      console.log("Erro: Consulta não realizada", err);
+      res.send('Login failed');
+    }
+  });
+});
+
+
+
 app.listen(3000, () => {
   console.log('Servidor rodando na porta 3000!')
 })
